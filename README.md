d97528b3d3aec9c80721e5c2a36e933c6d37dd5d
simonhowlett <simonjhowlett@gmail.com>
2020-06-15 20:45:51 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Just some workflow work to get a testing architecture proven...
+Testing Jira>Github>Travis CI/Circle>GCP integrations.
+
+Or Something.
+
+# ci-setup
---
+++ test/test_math.py
@@ -0,0 +1,18 @@
+import unittest
+
+def IsOdd(n):
+    return n % 2 == 1
+
+class IsOddTests(unittest.TestCase):
+
+    def testOne(self):
+        self.assertTrue(IsOdd(1))
+
+    def testtwo(self):
+        self.assertFalse(IsOdd(2))
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
