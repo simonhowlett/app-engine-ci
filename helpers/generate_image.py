b6069223e7c8a6f99eb6facf25a6ae19219e7bef
simonhowlett <simonjhowlett@gmail.com>
2020-07-07 18:28:51 -0700

    Updated, but not fixed helper script.

--- helpers/generate_image.py
+++ helpers/generate_image.py
@@ -13,29 +13,35 @@ Possible Enhancements:
 '''
 
 # ToDo: Bug: Escape the { in the f string correctly
-# To use: add pairs to the dictionary, run the file in the terminal and copy the html output.
+# To use: add pairs to the dictionary, run in the terminal and copy html
 # Format: "caption":"filename"
 
 
 _images = {"Unknown, Paris 2018": "paris_img_1", "caption2": "filename2"}
 
-for caption, filename in _images.items():
-    print((f'''<div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <img src="{{ url_for('static', filename='images/thumbnails/t_{filename}.jpg') }}"
-                        width="100%" height="100%" />
-                        <title>{caption}</title>
-                        <div class="card-body">
-                            <p class="card-text">{caption}</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="image/?image={filename}&caption={caption}">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" id="{filename}">
-                                    View Full Size</button></a>
+
+def gen_images():
+
+    for caption, filename in _images.items():
+        print((f'''<div class="col-md-4">
+                        <div class="card mb-4 shadow-sm">
+                            <img src="{{ url_for('static', filename='images/thumbnails/t_{filename}.jpg') }}"
+                            width="100%" height="100%" />
+                            <title>{caption}</title>
+                            <div class="card-body">
+                                <p class="card-text">{caption}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="image/?image={filename}&caption={caption}">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary" id="{filename}">
+                                        View Full Size</button></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-'''))
+    '''))
+
+
+gen_images()
