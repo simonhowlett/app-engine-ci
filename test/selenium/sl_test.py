6659c42b9f3085eb8f1a77741bbfdd30e8f1d5c8
simonhowlett <simonjhowlett@gmail.com>
2020-07-10 18:25:37 -0700

    Updating Test to check debug content is hidden/visible

--- test/selenium/sl_test.py
+++ test/selenium/sl_test.py
@@ -19,11 +19,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 
 # Local Url http://127.0.0.1:8080/
 # App Engine Url: TODO
 
 test_url = "http://127.0.0.1:8080/"
+debug_url = test_url + "?debug=true"
 
 
 class simple_tests(unittest.TestCase):
@@ -40,7 +42,7 @@ class simple_tests(unittest.TestCase):
         driver.get(test_url)
         driver.find_element_by_id('la_2019').click()
         alert_obj = driver.switch_to.alert
-        self.assertEqual('Not Yet wired up', alert_obj.text)
+        self.assertEqual('Intentionally Not Yet wired up', alert_obj.text)
         alert_obj.accept()
 
     def test_select_image_caption(self):
@@ -52,16 +54,34 @@ class simple_tests(unittest.TestCase):
         driver.find_element_by_id('paris_01').click()
         self.assertIn("Full Size Image", driver.title)
         caption = driver.find_element_by_name('caption')
-        self.assertEqual("'Unknown, Paris, 2018'", caption.text)
+        self.assertEqual('Unknown, Paris, 2018', caption.text)
         driver.find_element_by_name('back_to_home').click()
         self.assertIn("Homepage", driver.title)
+
+    def test_debug(self):
+        '''Tests Debug Text Appears in debug mode
+
+        '''
+        driver = self.driver
+        driver.get(debug_url)
+        debug_content = driver.find_element_by_class_name('hideme')
+        self.assertTrue(debug_content.is_displayed())
+
+    def test_not_debug(self):
+        '''Tests Debug Text hidden in normal use
+
+        '''
+        driver = self.driver
+        driver.get(test_url)
+        debug_content = driver.find_element_by_class_name('hideme')
+        self.assertFalse(debug_content.is_displayed())
 
         # TODO - Image URL check via parmameter
         time.sleep(2)
 
     def tearDown(self):
         self.driver.close()
-
+        
 
 # next lines run the tests as a suite, outputting each test's results.
 suite = unittest.TestLoader().loadTestsFromTestCase(simple_tests)
