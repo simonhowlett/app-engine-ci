9e24c71be6b06d3761ce47a20dab3c69640bd38a
Simon Howlett <simonjhowlett@gmail.com>
2024-10-29 18:24:30 -0400

    Update test_examples.py

--- test/test_examples.py
+++ test/test_examples.py
@@ -1,8 +1,8 @@
 import unittest
 import sys
 import os
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'helpers')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'helpers')))
 
 # from main import app
 from test import to_test
